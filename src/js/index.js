06fdeb0eee17d13974e344f0ea1477a04da8ebdb
Anton Liubimov <ant.liubimov@gmail.com>
2022-11-18 11:53:55 +0000

    feat: install yup and on-change

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
+import onChange from 'on-change';
+import yup from 'yup';
+
+const state = {
+  rssLink: '',
+  feeds: [],
+  posts: [],
+};
+
+const rssForm = document.querySelector('.rss-form');
+const urlInput = document.querySelector('#url-input');
