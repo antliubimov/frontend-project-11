b1b73f6762d5ce435ea6fd40abc811169947a4a8
Anton Liubimov <ant.liubimov@gmail.com>
2023-06-13 17:46:46 +0500

    feat: added rss validation

--- src/js/app.js
+++ src/js/app.js
@@ -2,42 +2,109 @@ import * as yup from 'yup';
 import watch from './view.js';
 
 export default async () => {
+  //   , t = {
+  //
+  //   modal: {
+  //     postId: null
+  //   },
+  //   ui: {
+  //     seenPosts: new Set
+  //   }
+  // }
   const state = {
+    rssForm: {
+      status: 'filling',
+      valid: false,
+      error: null,
+    },
     rssLink: '',
+    rssLinks: [],
     feeds: [],
     posts: [],
+    loadingProcess: {
+      status: 'idle',
+      error: null,
+    },
   };
 
   const elements = {
     rssForm: document.querySelector('.rss-form'),
-    fields: {},
-    errorFields: {},
+    input: document.querySelector('.rss-form input'),
+    feedback: document.querySelector('.feedback'),
+    feedsBox: document.querySelector('.feeds'),
+    postsBox: document.querySelector('.posts'),
+    submit: document.querySelector('.rss-form button[type="submit"]'),
+    modal: document.querySelector('#modal'),
   };
 
   // Неизвестная ошибка. Что-то пошло не так.
-  // Ресурс не содержит валидный RSS
-  // RSS успешно загружен
 
-  const rssSchema = yup.object({
-    url: yup.string().required().url(),
-  });
+  const { rssForm, rssLinks } = watch(elements, state);
 
-  const watchedState = watch(elements, state);
+  const rssSchema = yup.string().url('Ресурс не содержит валидный RSS'); // .notOneOf(watchedState.rssLinks, 'RSS уже существует');
 
-  elements.rssForm.addEventListener('submit', async (e) => {
+  elements.rssForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const newRSS = Object.fromEntries(formData);
-    try {
-      await rssSchema.validate(newRSS);
-      watchedState.rssForm.valid = true;
-      watchedState.rssForm.errors = [];
-    } catch (err) {
-      const errors = err.inner.reduce((acc, currErr) => {
-        const { path, message } = currErr;
-        return { ...acc, [path]: [...(acc[path] || []), message] };
-      }, {});
-      watchedState.rssForm.errors = errors;
-    }
+    const newRSS = new FormData(e.target).get('url');
+    return rssSchema.notOneOf(rssLinks, 'RSS уже существует').validate(newRSS)
+      .then((rss) => {
+        rssForm.valid = true;
+        rssLinks.push(rss);
+        rssForm.error = null;
+      })
+      .catch((err) => {
+        rssForm.valid = false;
+        rssForm.error = err.message;
+      });
+
+    // (t, watchedState.feeds).then((e=>{
+    //     e ? watchedState.form = {
+    //       ...watchedState.form,
+    //       valid: false,
+    //       error: e.key
+    //     } : (watchedState.form = {
+    //       ...watchedState.form,
+    //       valid: true,
+    //       error: null
+    //     },
+    //       ((e,t)=>{
+    //           e.loadingProcess.status = "loading";
+    //           const n = jo(t);
+    //           Ci.get(n, {
+    //             timeout: 1e4
+    //           }).then((n=>{
+    //               const r = Hs(n.data.contents)
+    //                 , i = {
+    //                 url: t,
+    //                 id: Ai(),
+    //                 title: r.title,
+    //                 description: r.descrpition
+    //               }
+    //                 , s = r.items.map((e=>({
+    //                 ...e,
+    //                 channelId: i.id,
+    //                 id: Ai()
+    //               })));
+    //               e.posts.unshift(...s),
+    //                 e.feeds.unshift(i),
+    //                 e.loadingProcess.error = null,
+    //                 e.loadingProcess.status = "idle",
+    //                 e.form = {
+    //                   ...e.form,
+    //                   status: "filling",
+    //                   error: null
+    //                 }
+    //             }
+    //           )).catch((t=>{
+    //               console.log(t),
+    //  e.loadingProcess.error =
+    // (e=>e.isParsingError ? "noRss" : e.isAxiosError ? "network" : "unknown")(t),
+    //                 e.loadingProcess.status = "failed"
+    //             }
+    //           ))
+    //         }
+    //       )(s, t))
+    //   }
+    // ))
   });
 };
--- src/js/view.js
+++ src/js/view.js
@@ -1,6 +1,35 @@
 import onChange from 'on-change';
-export default (state) => {
-   const watchedState = onChange(state, (path, value) => {
 
-   });
-}
+export default (elements, state) => {
+  // const { form, feedback, feeds, posts } = elements;
+  const { feedback } = elements;
+
+  const urlInput = document.querySelector('#url-input');
+  const handleErrors = () => {
+    feedback.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.innerText = [state.rssForm.error];
+    urlInput.classList.remove('is-valid');
+    urlInput.classList.add('is-invalid');
+  };
+
+  const handleValid = () => {
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
+    feedback.innerText = 'RSS успешно загружен';
+    urlInput.classList.remove('is-invalid');
+    urlInput.value = '';
+  };
+
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'rssForm.error') {
+      if (!value) {
+        handleValid();
+      } else {
+        handleErrors();
+      }
+    }
+  });
+
+  return watchedState;
+};
