0d56d743008d338d1f920416abbc256b3648efba
Anton Liubimov <ant.liubimov@gmail.com>
2023-06-15 17:07:19 +0500

    feat: added language's locales

---
+++ src/js/locales/ru.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    components: {
+      heading: 'RSS агрегатор',
+      lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      urlLabel: 'Ссылка RSS',
+      button: 'Добавить',
+      feeds: 'Фиды',
+      posts: 'Посты',
+      preview: 'Просмотр',
+    },
+    loadingSuccess: 'RSS успешно загружен',
+    errors: {
+      exists: 'RSS уже существует',
+      required: 'Не должно быть пустым',
+      notUrl: 'Ссылка должна быть валидным URL',
+      noRss: 'Ресурс не содержит валидный RSS',
+      network: 'Ошибка сети',
+      unknown: 'Неизвестная ошибка. Что-то пошло не так.',
+    },
+  },
+};
